96c3ae6e7f38ac35ecea04a851d595967422ee45
Dado Sutter <dadosutter@gmail.com>
2011-01-23 22:33:26 -0200

    Initial commit

---
+++ ipeditorini.lua
@@ -0,0 +1,22 @@
+IP = { 
+  addr = '139.082.143.059',
+  x = 25,
+  y = 20,
+  lvl = 12
+}
+
+title = { 
+  text = 'Ping Box', 
+  x = 40, 
+  y = 0,
+  lvl = 12,
+}
+
+edit = {
+  lvldim = 7, 
+  lvlcurrent = 12,
+}
+
+
+
+
